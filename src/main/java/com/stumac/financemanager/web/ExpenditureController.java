b7704a06deb7699cffdc24a5fd7ddf642b0d3dd0
Stuart McGregor <stumacsolutions@gmail.com>
2016-12-30 21:29:43 +0000

    Making controller package protected.

--- src/main/java/com/stumac/financemanager/web/ExpenditureController.java
+++ src/main/java/com/stumac/financemanager/web/ExpenditureController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
-public class ExpenditureController {
+class ExpenditureController {
 
     private final ExpenditureService service;
 
