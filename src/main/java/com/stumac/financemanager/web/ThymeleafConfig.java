3f12b30986e02b2e68918c1b531fcb46f5bc0f2e
Stuart McGregor <stumacsolutions@gmail.com>
2016-12-30 21:28:39 +0000

    Incorporating html5 thymeleaf dialect to perform client side validation.

---
+++ src/main/java/com/stumac/financemanager/web/ThymeleafConfig.java
@@ -0,0 +1,14 @@
+package com.stumac.financemanager.web;
+
+import net.sourceforge.html5val.Html5ValDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class ThymeleafConfig {
+
+    @Bean
+    Html5ValDialect html5ValDialect() {
+        return new Html5ValDialect();
+    }
+}
--- src/main/java/com/stumac/financemanager/web/ViewControllerConfig.java
+++ src/main/java/com/stumac/financemanager/web/ViewControllerConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-class ViewControllerConfiguration extends WebMvcConfigurerAdapter {
+class ViewControllerConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
