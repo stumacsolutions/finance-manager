21851cb450fc4aca7188432521ff86bcc954b352
Stuart McGregor <stumacsolutions@gmail.com>
2016-12-30 12:57:59 +0000

    Adding application configuration class.

--- src/main/java/com/stumac/financemanager/Application.java
+++ src/main/java/com/stumac/financemanager/Application.java
@@ -2,8 +2,10 @@ package com.stumac.financemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ApplicationConfig.class})
 public class Application {
 
     public static void main(String[] args) {
