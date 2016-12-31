61351dd4bcdbf32570aed0ce77fcaafe15247aa7
Stuart McGregor <stumacsolutions@gmail.com>
2016-12-31 21:21:29 +0000

    Overriding DigitsPerformer from html5valdialect library.

---
+++ src/main/java/net/sourceforge/html5val/performers/DigitsPerformer.java
@@ -0,0 +1,18 @@
+package net.sourceforge.html5val.performers;
+
+import net.sourceforge.html5val.ValidationPerformer;
+import org.thymeleaf.dom.Element;
+
+import javax.validation.constraints.Digits;
+import java.math.BigDecimal;
+
+public class DigitsPerformer implements ValidationPerformer<Digits> {
+
+    public Class<Digits> getConstraintClass() {
+        return Digits.class;
+    }
+
+    public void putValidationCodeInto(Digits digits, Element element) {
+        element.setAttribute("step", BigDecimal.valueOf(1, digits.fraction()).toString());
+    }
+}
