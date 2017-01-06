727184f7e6e0bb341938f43fd15957f287929f38
Stuart McGregor <stuart.macgregor@ros.gov.uk>
2017-01-06 09:19:08 +0000

    Extracting reusable logic in controller layer.

--- src/main/java/com/stumac/financemanager/web/ExpenditureController.java
+++ src/main/java/com/stumac/financemanager/web/ExpenditureController.java
@@ -3,78 +3,23 @@ package com.stumac.financemanager.web;
 import com.stumac.financemanager.data.expenditure.ExpenditureCategory;
 import com.stumac.financemanager.service.expenditure.Expenditure;
 import com.stumac.financemanager.service.expenditure.ExpenditureService;
-import com.stumac.financemanager.web.exception.ResourceNotFoundException;
-import lombok.RequiredArgsConstructor;
+import com.stumac.financemanager.web.common.UserDataController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
-import java.util.Optional;
-
 @Controller
-@RequiredArgsConstructor
 @RequestMapping(path = "/expenditure")
-class ExpenditureController {
+class ExpenditureController extends UserDataController<Expenditure> {
 
-    private final ExpenditureService service;
-
-    @GetMapping(path = "/manage")
-    public String manage(Model model) {
-        model.addAttribute("expenditures", service.listAll());
-        return "expenditure/manage";
-    }
-
-    @GetMapping(path = "/add")
-    public String add(Model model) {
-        model.addAttribute("expenditure", new Expenditure());
-        return "expenditure/edit";
-    }
-
-    @GetMapping(path = "/edit/{id}")
-    public String edit(@PathVariable(name = "id") long id, Model model) {
-        model.addAttribute("expenditure", getExpenditure(id));
-        return "expenditure/edit";
-    }
-
-    @GetMapping(path = "/delete/{id}")
-    public String delete(@PathVariable(name = "id") long id, Model model) {
-        model.addAttribute("expenditure", getExpenditure(id));
-        return "expenditure/delete";
-    }
-
-    @PostMapping(path = "/delete/{id}")
-    public String remove(@PathVariable(name = "id") long id) {
-        service.delete(id);
-        return "redirect:/expenditure/manage";
-    }
-
-    @PostMapping(path = "/save")
-    public String save(@Valid Expenditure expenditure, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("expenditure", expenditure);
-            return "expenditure/edit";
-        } else {
-            service.add(expenditure);
-            return "redirect:/expenditure/manage";
-        }
+    @Autowired
+    public ExpenditureController(ExpenditureService service) {
+        super(service, Expenditure.class);
     }
 
     @ModelAttribute("categories")
     public ExpenditureCategory[] getCategories() {
         return ExpenditureCategory.values();
-    }
-
-    private Expenditure getExpenditure(long id) {
-        Optional<Expenditure> maybeExpenditure = service.get(id);
-        if (maybeExpenditure.isPresent()) {
-            return maybeExpenditure.get();
-        }
-        throw new ResourceNotFoundException();
     }
 }
---
+++ src/main/java/com/stumac/financemanager/web/common/UserDataController.java
@@ -0,0 +1,78 @@
+package com.stumac.financemanager.web.common;
+
+import com.stumac.financemanager.service.common.UserData;
+import com.stumac.financemanager.service.common.UserDataService;
+import com.stumac.financemanager.web.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+import static java.lang.String.format;
+
+@RequiredArgsConstructor
+public abstract class UserDataController<D extends UserData> {
+
+    private final UserDataService<D> service;
+    private final Class<D> serviceClass;
+
+    @GetMapping(path = "/manage")
+    public String manage(Model model) {
+        model.addAttribute(format("%ss", getType()), service.listAll());
+        return format("%s/manage", getType());
+    }
+
+    @SneakyThrows
+    @GetMapping(path = "/add")
+    public String add(Model model) {
+        model.addAttribute(getType(), serviceClass.newInstance());
+        return format("%s/edit", getType());
+    }
+
+    @GetMapping(path = "/edit/{id}")
+    public String edit(@PathVariable(name = "id") long id, Model model) {
+        model.addAttribute(getType(), getUserData(id));
+        return format("%s/edit", getType());
+    }
+
+    @GetMapping(path = "/delete/{id}")
+    public String delete(@PathVariable(name = "id") long id, Model model) {
+        model.addAttribute(getType(), getUserData(id));
+        return format("%s/delete", getType());
+    }
+
+    @PostMapping(path = "/delete/{id}")
+    public String remove(@PathVariable(name = "id") long id) {
+        service.delete(id);
+        return format("redirect:/%s/manage", getType());
+    }
+
+    @PostMapping(path = "/save")
+    public String save(@Valid D userData, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute(getType(), userData);
+            return format("%s/edit", getType());
+        } else {
+            service.add(userData);
+            return format("redirect:/%s/manage", getType());
+        }
+    }
+
+    private String getType() {
+        return serviceClass.getSimpleName().toLowerCase();
+    }
+
+    private D getUserData(long id) {
+        Optional<D> maybeUserData = service.get(id);
+        if (maybeUserData.isPresent()) {
+            return maybeUserData.get();
+        }
+        throw new ResourceNotFoundException();
+    }
+}
