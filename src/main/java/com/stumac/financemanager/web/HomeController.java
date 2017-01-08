14626ab45339d072e250b8f7de11de3f72f3dbb3
Stuart McGregor <stumacsolutions@gmail.com>
2017-01-08 19:34:51 +0000

    Fixing broken highlighting of active navigation section.

---
+++ src/main/java/com/stumac/financemanager/web/HomeController.java
@@ -0,0 +1,19 @@
+package com.stumac.financemanager.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(path = "/")
+    public String home() {
+        return "index";
+    }
+
+    @ModelAttribute("navigationSection")
+    public String getNavigationSection() {
+        return "home";
+    }
+}
--- src/main/java/com/stumac/financemanager/web/common/UserDataController.java
+++ src/main/java/com/stumac/financemanager/web/common/UserDataController.java
@@ -8,6 +8,7 @@ import lombok.SneakyThrows;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -62,6 +63,11 @@ public abstract class UserDataController<D extends UserData> {
             service.add(userData);
             return format("redirect:/%s/manage", getType());
         }
+    }
+
+    @ModelAttribute("navigationSection")
+    public String getNavigationSection() {
+        return getType();
     }
 
     private String getType() {
