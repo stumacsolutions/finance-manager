347def5c88901d41d90bd58b1a5d4d1aeecc5b79
Stuart McGregor <stumacsolutions@gmail.com>
2017-01-21 11:34:53 +0000

    Adding additional expenditure categories.

--- src/main/java/com/stumac/financemanager/data/expenditure/ExpenditureCategory.java
+++ src/main/java/com/stumac/financemanager/data/expenditure/ExpenditureCategory.java
@@ -8,10 +8,16 @@ import lombok.RequiredArgsConstructor;
 public enum ExpenditureCategory {
 
     BOOKS("Books"),
+    CORPORATION_TAX("Corporation Tax"),
+    DIVIDEND("Dividend"),
+    EMPLOYER_PAYE("PAYE for Employers"),
     HOME_OFFICE("Home Office"),
     MILEAGE("Mileage"),
+    PENSION("Pension"),
     PHONE_BILL("Phone Bill"),
-    TRAIN_TICKET("Train Ticket");
+    SALARY("Salary"),
+    TRAIN_TICKET("Train Ticket"),
+    VAT("VAT");
 
     private final String displayName;
 
