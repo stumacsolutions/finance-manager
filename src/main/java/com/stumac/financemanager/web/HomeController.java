7d024b71a857b827fef9f0b37c16788db108c511
Stuart McGregor <stumacsolutions@gmail.com>
2017-01-31 21:28:13 +0000

    Making controller class package protected.

--- src/main/java/com/stumac/financemanager/web/HomeController.java
+++ src/main/java/com/stumac/financemanager/web/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-public class HomeController {
+class HomeController {
 
     @GetMapping(path = "/")
     public String home() {
