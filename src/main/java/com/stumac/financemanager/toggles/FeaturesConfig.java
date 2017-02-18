fd64f751eb3d5ed0dcac468311fbe601bac6b39d
Stuart McGregor <stumacsolutions@gmail.com>
2017-02-18 13:54:51 +0000

    Exposing feature manager as a bean.

--- src/main/java/com/stumac/financemanager/toggles/FeaturesConfig.java
+++ src/main/java/com/stumac/financemanager/toggles/FeaturesConfig.java
@@ -3,15 +3,23 @@ package com.stumac.financemanager.toggles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.togglz.core.manager.EnumBasedFeatureProvider;
+import org.togglz.core.manager.FeatureManager;
 import org.togglz.core.repository.StateRepository;
 import org.togglz.core.repository.jdbc.JDBCStateRepository;
 import org.togglz.core.spi.FeatureProvider;
 
 import javax.sql.DataSource;
 
+import static org.togglz.core.context.FeatureContext.getFeatureManager;
+
 @Configuration
 class FeaturesConfig
 {
+    @Bean
+    FeatureManager featureManager() {
+        return getFeatureManager();
+    }
+
     @Bean
     FeatureProvider featureProvider()
     {
